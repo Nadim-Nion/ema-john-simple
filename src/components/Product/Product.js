e4dbfeb1349e53134080a114fd3d86d6aab7080a
Nadim Mahmud Nion <nadim15-1746@diu.edu.bd>
2023-10-08 17:22:26 +0600

    first commit

---
+++ src/components/Product/Product.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import './Product.css';
+
+const Product = (props) => {
+    // console.log(props.product);
+    console.log(props);
+
+    /* const { img, name, price, seller, ratings } = props.product;
+    const { handleAddToCart } = props; */
+
+
+    const { product, handleAddToCart } = props;
+    const { img, name, price, seller, ratings } = product;
+
+    return (
+        <div className='product'>
+            <img src={img} alt="" />
+            <div className='product-info'>
+                <p className='product-name'>{name}</p>
+                <p>Price: ${price}</p>
+                <p><small>Manufacturer: {seller}</small></p>
+                <p><small>Ratings: {ratings} stars</small></p>
+            </div>
+
+            <button onClick={() => handleAddToCart(product)} className='btn-cart'>
+                <p>Add to cart</p>
+                <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
+            </button>
+
+        </div>
+    );
+};
+
+export default Product;
